d6b4d088fb1a15a4372ad9f5ad9105428463459b
firdausmaulana <firdausmaulana@akupintar.id>
2024-03-30 22:31:42 +0900

    Update

---
+++ ver_1_1/dictionary_feedback.py
@@ -0,0 +1,11 @@
+# Get words about feedback
+def get_feedback_words():
+    words = {
+        -1: "",
+        0: "Your back is bent forward too much, you can do this by bending your back slightly backward. About 10 - 50 degrees from the back when standing straight.",
+        1: "Your back is bent backward too much, you can do this by bending your back slightly forward. Make sure not to bend your back backwards as this can cause injury.",
+        2: "In doing squats, keeping your thighs from dropping lower than your knees is key to avoiding injury. If your thighs drop too low, it could indicate that you need to strengthen your core and thigh muscles, as well as improve balance and flexibility.",
+        3: "When doing squats, it is important to keep the body from dropping down too much. Make sure that your pelvis does not drop beyond the knee position.",
+    }
+
+    return words
