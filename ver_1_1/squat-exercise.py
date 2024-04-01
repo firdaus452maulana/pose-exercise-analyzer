e31774b673f7e0cf8096a1f2fc6b4947a55440b9
firdaus452maulana <firdaus452maulana@gmail.com>
2024-04-01 16:51:43 +0900

    Update resize frame ad new directory

--- ver_1_1/squat-exercise.py
+++ ver_1_1/squat-exercise.py
@@ -1,9 +1,8 @@
 import cv2
-import csv
 import datetime
 import os
 
-from utils import get_mediapipe_pose
+from utils import get_mediapipe_pose, create_csv_files
 from squat_process import ProcessFrame
 from thresholds import get_thresholds_beginner
 from dictionary_feedback import get_feedback_words
@@ -12,19 +11,23 @@ from dictionary_feedback import get_feedback_words
 thresholds = get_thresholds_beginner()
 
 name_folder = os.path.join(str(datetime.date.today()), "set4")
-if not os.path.exists(str(name_folder)):
-    os.mkdir(str(name_folder))
+if not os.path.exists(str(datetime.date.today())):
+    os.mkdir(str(datetime.date.today()))
+if not os.path.exists(name_folder):
+    os.mkdir(name_folder)
 
 live_process_frame = ProcessFrame(thresholds=thresholds, flip_frame=True, name_folder=name_folder)
 # Initialize face mesh solution
 pose = get_mediapipe_pose()
 
-cap = cv2.VideoCapture("day1/WIN_20240401_10_28_42_Pro.mp4")
+cap = cv2.VideoCapture("day1/WIN_20240401_15_54_08_Pro.mp4")
 
 while True:
     success, img = cap.read()
     if not success:
         break
+    if img.shape[1] > 1000:
+        img = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)), interpolation = cv2.INTER_AREA)
     frame, _ = live_process_frame.process(img, pose)  # Process frame
 
     cv2.imshow("Image", frame)
@@ -33,32 +36,4 @@ while True:
 
 feedback_words = get_feedback_words()
 
-state_rep = live_process_frame.state_tracker['STATE_REP']
-data_feedback = live_process_frame.DATA_FEEDBACK
-write_csv = [['repetition', 'state', 'feedback_1', 'feedback_2', 'feedback_3', 'feedback_4']]
-for rep in range(len(state_rep)):
-    # print(data_feedback)
-    if state_rep[rep] == 'FAILED':
-        write_csv.append([rep + 1, state_rep[rep], 'You are performing the squat imperfectly, you should lower the '
-                                                   'squat until your thighs are in line with your knees.', '', '', ''])
-    elif state_rep[rep] == 'IMPROPER':
-        feedback = data_feedback[rep + 1]
-        feedback_array = [-1, -1, -1, -1]
-        for x in range(len(feedback)):
-            feedback_array[x] = list(feedback)[x]
-        write_csv.append([rep + 1, state_rep[rep], feedback_words[feedback_array[0]], feedback_words[feedback_array[1]],
-                          feedback_words[feedback_array[2]], feedback_words[feedback_array[3]]])
-
-    else:
-        write_csv.append([rep + 1, state_rep[rep], 'You are strong, you are capable, and every squat is a testament '
-                                                   'to your dedication and hard work. Keep moving, and let every '
-                                                   'squat lift your spirit higher!', '', '', ''])
-
-print(live_process_frame.DATA_FOR_CSV)
-f = open('{}/data_feedback_squats.csv'.format(name_folder), 'w')
-
-writer = csv.writer(f)
-
-writer.writerows(write_csv)
-
-f.close()
+create_csv_files(live_process_frame, feedback_words, name_folder)
