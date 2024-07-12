07ae145d1c233a59534f67e1a19a0cd09c0b0d07
firdausmaulana <firdausmaulana@akupintar.id>
2024-07-12 13:48:13 +0900

    Update threshold for beginner

--- ver_1_1/thresholds.py
+++ ver_1_1/thresholds.py
@@ -3,7 +3,7 @@ def get_thresholds_beginner():
     _ANGLE_HIP_KNEE_VERT = {
         'NORMAL': (0, 32),
         'TRANS': (35, 65),
-        'PASS': (85, 95)
+        'PASS': (80, 95)
     }
 
     thresholds = {
@@ -11,7 +11,7 @@ def get_thresholds_beginner():
 
         'HIP_THRESH': [10, 50],
         'ANKLE_THRESH': 45,
-        'KNEE_THRESH': [50, 85, 95],
+        'KNEE_THRESH': [50, 80, 95],
 
         'OFFSET_THRESH': 35.0,
         'INACTIVE_THRESH': 15.0,
