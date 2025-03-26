b42627d5e34fa458d3e53e8ccb224b54fa06d6a4
firdaus452maulana <firdaus452maulana@gmail.com>
2025-03-26 13:36:39 +0900

    Add record feature

--- ver_1_1/squat-exercise.py
+++ ver_1_1/squat-exercise.py
@@ -2,7 +2,7 @@ import cv2
 import datetime
 import os
 
-from utils import get_mediapipe_pose, create_csv_files, create_dict_object
+from utils import get_mediapipe_pose, create_csv_files, create_dict_object, save_to_db
 from squat_process import ProcessFrame
 from thresholds import get_thresholds_beginner
 from dictionary_feedback import get_feedback_words
@@ -10,7 +10,7 @@ from dictionary_feedback import get_feedback_words
 # thresholds = get_thresholds_beginner()
 thresholds = get_thresholds_beginner()
 
-name_folder = os.path.join(str(datetime.date.today()), "set4")
+name_folder = os.path.join(str(datetime.date.today()), "set1")
 if not os.path.exists(str(datetime.date.today())):
     os.mkdir(str(datetime.date.today()))
 if not os.path.exists(name_folder):
@@ -20,12 +20,21 @@ live_process_frame = ProcessFrame(thresholds=thresholds, flip_frame=True, name_f
 # Initialize face mesh solution
 pose = get_mediapipe_pose()
 
-cap = cv2.VideoCapture("WIN_20240330_23_27_27_Pro.mp4")
+cap = cv2.VideoCapture(1)
+
+# Get the default frame width and height
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output.mp4', fourcc, 25.0, (frame_width, frame_height))
 
 while True:
     success, img = cap.read()
     if not success:
         break
+    # out.write(img)
     frame, _ = live_process_frame.process(img, pose)  # Process frame
 
     cv2.imshow("Image", frame)
@@ -33,7 +42,8 @@ while True:
         break
 
 feedback_words = get_feedback_words()
+# out.release()
 create_dict_object(live_process_frame, name_folder)
+# save_to_db(live_process_frame, name_folder)
 
-
-create_csv_files(live_process_frame, feedback_words, name_folder)
+# create_csv_files(live_process_frame, feedback_words, name_folder)
